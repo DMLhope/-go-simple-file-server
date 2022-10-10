74efda3cac64fa5707c6178a8f10a8f8dfbc2515
daimingliang <daimingliang@uniontech.com>
2022-10-10 17:26:53 +0800

    添加接收命令行参数

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
+)
+
+var (
+	ip   string
+	prot int
+	path string
+	help bool
 )
 
 type Server_info struct {
@@ -12,27 +21,36 @@ type Server_info struct {
 }
 
 func (info *Server_info) address() string {
-	if info.ip == "" || info.prot == 0 {
 
-		info.ip = "127.0.0.1"
-		info.prot = 9000
-	}
 	// url := info.ip + ":" + strconv.Itoa(info.prot)
 	url := fmt.Sprintf("%s:%d", info.ip, info.prot)
-	fmt.Printf("服务地址设定为:%s\n", url)
+	fmt.Printf("启动服务，地址为:%s\n", url)
 	return url
 }
 
 func main() {
+	init_flag()
 	server_info := Server_info{
-		ip:   "127.0.0.1",
-		prot: 9000,
-		path: "./",
+		ip:   ip,
+		prot: prot,
+		path: path,
 	}
 
 	http.Handle("/", http.FileServer(http.Dir(server_info.path)))
 	e := http.ListenAndServe(server_info.address(), nil)
 	if e != nil {
 		fmt.Println(e.Error())
+	}
+}
+
+func init_flag() {
+	flag.StringVar(&ip, "addr", "127.0.0.1", "ip地址,默认为127.0.0.1")
+	flag.IntVar(&prot, "p", 9000, "端口号,默认为9000")
+	flag.StringVar(&path, "path", "./", "路径,默认为./当前路径")
+	flag.BoolVar(&help, "h", false, "帮助")
+	flag.Parse()
+	if help {
+		flag.PrintDefaults()
+		os.Exit(0)
 	}
 }
